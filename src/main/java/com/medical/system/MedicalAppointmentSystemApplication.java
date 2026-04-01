4bb14274d6c0a660efd2c6cbb70ad888791b054f
Volkovich81 <volkovicnika@gmail.com>
2026-04-01 19:15:29 +0300

    chore: final coverage fix

--- src/main/java/com/medical/system/MedicalAppointmentSystemApplication.java
+++ src/main/java/com/medical/system/MedicalAppointmentSystemApplication.java
@@ -3,10 +3,8 @@ package com.medical.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@lombok.Generated
 @SpringBootApplication
 public class MedicalAppointmentSystemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MedicalAppointmentSystemApplication.class, args);
     }
