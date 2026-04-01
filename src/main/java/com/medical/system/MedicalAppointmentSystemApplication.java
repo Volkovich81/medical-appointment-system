29fc4a83f9117667929de9319763144c4b87063d
Volkovich81 <volkovicnika@gmail.com>
2026-04-01 18:42:08 +0300

    trigger: force sonar coverage recalculation

--- src/main/java/com/medical/system/MedicalAppointmentSystemApplication.java
+++ src/main/java/com/medical/system/MedicalAppointmentSystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedicalAppointmentSystemApplication {
+    @lombok.Generated
     public static void main(String[] args) {
         SpringApplication.run(MedicalAppointmentSystemApplication.class, args);
     }
