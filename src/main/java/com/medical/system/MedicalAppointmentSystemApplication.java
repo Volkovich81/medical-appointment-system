8f7fd5e003709babe55c78ce02d4875af8f6b9da
Volkovich81 <volkovicnika@gmail.com>
2026-04-01 19:43:35 +0300

    refactor: force clean build and generated tag

--- src/main/java/com/medical/system/MedicalAppointmentSystemApplication.java
+++ src/main/java/com/medical/system/MedicalAppointmentSystemApplication.java
@@ -3,6 +3,7 @@ package com.medical.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@lombok.Generated
 @SpringBootApplication
 public class MedicalAppointmentSystemApplication {
     public static void main(String[] args) {
